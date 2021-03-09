46566f91cad9b981d3aab4e51234414b5c99cdce
ChiSinChow <chisinchow@gmail.com>
2021-03-09 23:56:04 +0800

    Added customer feedback activity page 1

---
+++ app/src/main/java/com/example/newhopehotel/login/CustomerFeedback1.kt
@@ -0,0 +1,11 @@
+package com.example.newhopehotel.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomerFeedback1 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_customer_feedback1)
+    }
+}
