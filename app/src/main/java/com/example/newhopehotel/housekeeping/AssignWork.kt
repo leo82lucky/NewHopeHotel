8312d462982918b10824998817be89c1abb5a688
Jason <jsszsoon052@gmail.com>
2021-03-18 06:44:32 +0800

    Progress on Housekeeping Module

---
+++ app/src/main/java/com/example/newhopehotel/housekeeping/AssignWork.kt
@@ -0,0 +1,18 @@
+package com.example.newhopehotel.housekeeping
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.newhopehotel.R
+import kotlinx.android.synthetic.main.activity_assign_work_worker_list.*
+
+class AssignWork : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_assign_work_worker_list)
+        val layoutManager = LinearLayoutManager(this)
+        recyclerView.layoutManager = layoutManager
+        val adapter = WorkerListAdapter()
+        recyclerView.adapter = adapter
+    }
+}
