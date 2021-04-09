6650328266ccc828931560b71ec3b3d8ebffd493
PehKMinPhilip <pehkmp-wm18@student.tarc.edu.my>
2021-04-09 16:59:08 +0800

    Add Logo & Time Filter Function

--- app/src/main/java/com/example/newhopehotel/roomService/viewMorningCallList/MorningCallListFragment.kt
+++ app/src/main/java/com/example/newhopehotel/roomService/viewMorningCallList/MorningCallListFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import androidx.databinding.DataBindingUtil
@@ -12,12 +13,14 @@ import androidx.fragment.app.transaction
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.*
 import com.example.newhopehotel.R
+import com.example.newhopehotel.checkInCheckOut.AddCicoViewModel
 import com.example.newhopehotel.data.UIState
 import com.example.newhopehotel.database.CheckInCheckOutEntity
 import com.example.newhopehotel.database.MorningCallEntity
 import com.example.newhopehotel.databinding.FragmentMorningCallListBinding
 import com.example.newhopehotel.roomService.viewRoomServiceList.AddRoomServiceFragment
 import com.example.newhopehotel.roomService.viewRoomServiceList.CHOSEN_RS
+import kotlinx.android.synthetic.main.fragment_morning_call_list.*
 import org.jetbrains.anko.design.longSnackbar
 
 const val CHOSEN_MC = "chosenMC"
@@ -74,15 +77,85 @@ class MorningCallListFragment : Fragment(), MorningCallAdapter.MorningCallClickL
                 morningCallActivityViewModel.uiState.set(UIState.HAS_DATA)
                 mAdapter.morningCallList = morningCallEntries
                 mMorningCallList = morningCallEntries
+
+
             }
+        })
+        btn_8am.setOnClickListener({
+            Toast.makeText(activity, "8AM Morning Call List", Toast.LENGTH_SHORT).show()
+             filter8AMMc( mMorningCallList !!)
+            })
+        btn_9am.setOnClickListener({
+            Toast.makeText(activity, "9AM Morning Call List", Toast.LENGTH_SHORT).show()
+            filter9AMMc( mMorningCallList !!)
+        })
+        btn_10am.setOnClickListener({
+            Toast.makeText(activity, "10AM Morning Call List", Toast.LENGTH_SHORT).show()
+            filter10AMMc( mMorningCallList !!)
+        })
+        btn_11am.setOnClickListener({
+            Toast.makeText(activity, "11AM Morning Call List", Toast.LENGTH_SHORT).show()
+            filter11AMMc( mMorningCallList !!)
         })
 
 
+
     }
 
+    fun filter8AMMc( mcEntries:List<CheckInCheckOutEntity>)
+    {
+        val eightAM= mutableMapOf(
+            AddCicoViewModel.EIGHT_AM to true, AddCicoViewModel.NINE_AM to false,
+            AddCicoViewModel.TEN_AM to false, AddCicoViewModel.ELEVEN_AM to false
+        )
+        filterMCList(mcEntries,eightAM)
+    }
+    fun filter9AMMc( mcEntries:List<CheckInCheckOutEntity>)
+    {
+        val nineAM= mutableMapOf(
+            AddCicoViewModel.EIGHT_AM to false, AddCicoViewModel.NINE_AM to true,
+            AddCicoViewModel.TEN_AM to false, AddCicoViewModel.ELEVEN_AM to false
+        )
+        filterMCList(mcEntries,nineAM)
+    }
+    fun filter10AMMc( mcEntries:List<CheckInCheckOutEntity>)
+    {
+        val tenAM= mutableMapOf(
+            AddCicoViewModel.EIGHT_AM to false, AddCicoViewModel.NINE_AM to false,
+            AddCicoViewModel.TEN_AM to true, AddCicoViewModel.ELEVEN_AM to false
+        )
+        filterMCList(mcEntries,tenAM)
+    }
+    fun filter11AMMc( mcEntries:List<CheckInCheckOutEntity>)
+    {
+        val elevenAM= mutableMapOf(
+            AddCicoViewModel.EIGHT_AM to false, AddCicoViewModel.NINE_AM to false,
+            AddCicoViewModel.TEN_AM to false, AddCicoViewModel.ELEVEN_AM to true
+        )
+        filterMCList(mcEntries,elevenAM)
+    }
+    fun filterMCList(mcEntries:List<CheckInCheckOutEntity>, time:Map<String, Boolean>)
+    {
+        var tempMCList: MutableList<CheckInCheckOutEntity> = ArrayList()
+
+        for (item in mcEntries) {
+
+            if(item.morningCall==time)
+            {
+                tempMCList.add(item)
+            }
+        }
+        mAdapter.morningCallList = tempMCList
+        mAdapter.notifyDataSetChanged()
+    }
     override fun onMorningCallClicked(chosenToy: CheckInCheckOutEntity) {
 
     }
-
+    companion object {
+        const val EIGHT_AM = "8 AM"
+        const val NINE_AM = "9 AM"
+        const val TEN_AM = "10 AM"
+        const val ELEVEN_AM = "11 AM"
+    }
 
 }
