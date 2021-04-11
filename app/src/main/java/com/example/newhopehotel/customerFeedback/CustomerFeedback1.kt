53a4b2f928766c21bfa031c5a2894ecf80585e5a
ChiSinChow <chisinchow@gmail.com>
2021-04-11 14:01:31 +0800

    Customer Feedback: Pull purpose

--- app/src/main/java/com/example/newhopehotel/customerFeedback/CustomerFeedback1.kt
+++ app/src/main/java/com/example/newhopehotel/customerFeedback/CustomerFeedback1.kt
@@ -2,14 +2,27 @@ package com.example.newhopehotel.customerFeedback
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentManager
 import com.example.newhopehotel.R
 
 class CustomerFeedback1 : AppCompatActivity() {
+
+    val feedback = Feedback()
+    val feedbackEdit = FeedbackEdit()
+    val feedbackScene = FeedbackScene()
+    val viewedFeedback = ViewedFeedback()
+
+    
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_customer_feedback1)
     }
+
+    override fun getSupportFragmentManager(): FragmentManager {
+        return super.getSupportFragmentManager()
+    }
+
 
 
 }
